add350f38192d0d34a6bdabcd9f0d0abff8c854b
Edgardo Villafane <edgardovillafane@gmail.com>
2018-04-05 00:00:23 +0100

    decimales

--- php/calculate.php
+++ php/calculate.php
@@ -16,7 +16,7 @@ $sizes = [
 ];
 
 //this operation apply the size coeficient to a base price of a selected metal
-$price=round($metals[$_POST["metal"]] * $sizes[$_POST["size"]]);
+$price=round_to_2dp($metals[$_POST["metal"]] * $sizes[$_POST["size"]]);
 $myRing=new stdClass();
 $myRing->price = $price;
 $myRing->metal=$_POST["metal"];
